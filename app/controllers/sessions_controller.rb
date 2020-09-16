21af28fa23990a368c7ed2489c4ec465415d12f6
Terry Thompson <terry.lyn.thompson@gmail.com>
2020-09-16 12:21:59 -0500

    Changed user schema from 'name' to 'email'

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,13 +4,15 @@ class SessionsController < ApplicationController
   end
 
   def create
-    find_user
-    if @user && @user.authenticate(params[:session][:password])
-      session[:user_id] = @user.id
-      redirect_to user_path(@user)
-    else
-      render 'sessions/new'
+    @user = User.find_or_create_by(uid: auth['uid']) do |u|
+      u.name = auth['info']['name']
+      u.email = auth['info']['email']
+      u.image = auth['info']['image']
     end
+ 
+    session[:user_id] = @user.id
+ 
+    render '/'
   end
 
   def destroy
--- db/schema.rb
+++ db/schema.rb
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 2020_09_09_031733) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "email"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
