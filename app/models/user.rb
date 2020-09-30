83e8a4a73d3d9537a54de39c42fcce6090e88ed9
Terry Thompson <terry.lyn.thompson@gmail.com>
2020-09-30 06:46:00 -0500

    Added User validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
   has_secure_password
   has_many :recipes
   has_many :ingredients, through: :recipes
+
+  validates :password, presence: true
+  validates :username, presence: true, uniqueness: true
+  validates :email, uniqueness: true, :format => /\A[a-zA-Z0-9.!\#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*\z/
+
 end
