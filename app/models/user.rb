c2bd1cfe561fc2c52dfacb489d8cd5d63199df1e
Terry Thompson <terry.lyn.thompson@gmail.com>
2020-10-07 09:52:44 -0500

    Corrected Associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :recipes
-  has_many :ingredients, through: :recipes
+
 
   validates :password, presence: true
   validates :username, presence: true, uniqueness: true
