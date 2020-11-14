fc8419e2be530c84b3b854bf42480e5546b6a2df
Terry Thompson <terry.lyn.thompson@gmail.com>
2020-11-14 13:08:38 -0600

    Added name to users, modifications to Google Oauth

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_13_024615) do
+ActiveRecord::Schema.define(version: 2020_11_14_190705) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2020_11_13_024615) do
     t.string "google_token"
     t.string "google_refresh_token"
     t.integer "uid"
+    t.string "name"
   end
 
 end
